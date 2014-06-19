4904d80d3af50d1106f832c7683bd42aa2257599
Andrew Starovoyt <astarovoyt@naumen.ru>
2014-06-19 22:05:49 +0400

    add parsing§

---
+++ generator/src/kkey/generator/ParsingUtils.java
@@ -0,0 +1,30 @@
+package kkey.generator;
+
+
+import java.util.HashMap;
+
+public class ParsingUtils {
+
+  public static final HashMap<String, String> types = new HashMap<>();
+
+  static {
+    types.put("Function", "Function");
+    types.put("String", "String");
+    types.put("Number", "Number");
+    types.put("Number", "Number");
+    types.put("Boolean", "Boolean");
+    types.put("Array of Strings", "String[]");
+  }
+
+  public static String parseType(String type) {
+    return null;
+  }
+
+  public static boolean isFunction(String name) {
+    return -1 != name.indexOf('(') && -1 != name.indexOf(')');
+  }
+
+  public static String getNameSpace(String name) {
+    return Character.isUpperCase(name.charAt(0)) && name.indexOf('.') > 0 ? name.substring(0, name.indexOf('.') - 1) : null;
+  }
+}
