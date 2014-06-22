4793c61e68fa8fd7486b1a0e7705ded67ae9eb4a
Andrew Starovoyt <astarovoyt@naumen.ru>
2014-06-22 23:23:49 +0400

    args

--- generator/src/kkey/generator/ArgsParsing.java
+++ generator/src/kkey/generator/ArgsParsing.java
@@ -10,19 +10,42 @@ import kkey.generator.blocks.OptionDeclaration;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class ArgsParsing {
   public static Collection<Declaration> getArgs(JsonElement element) {
     Collection<Declaration> result = new ArrayList<>();
-    if (Generator.getElement(element, Generator.MEMBER_NAME).endsWith("()")) {
+    String fullName = Generator.getElement(element, Generator.MEMBER_NAME);
+    if (fullName.endsWith("()")) {
       return result;
     }
-    putArgsWithParams(element, result);
+    List<Declaration> directArgs = getArgsFromField(element);
+    boolean hasArgs = !directArgs.isEmpty();
+
+    if (hasArgs) {
+      result.addAll(directArgs);
+      for (String name : argsFromName(fullName)) {
+        if (isOption(name)) {
+          result.add(getOptions(element, name.contains("[")));
+        }
+      }
+    }
+    else {
+      for (String name : argsFromName(fullName)) {
+
+      }
+    }
+
     return result;
   }
 
-  public static void putArgsWithParams(JsonElement element, Collection<Declaration> result) {
+  public static List<Declaration> getArgsFromField(JsonElement element) {
+    List<Declaration> result = new ArrayList<>();
+
     JsonElement args = element.getAsJsonObject().get("args");
+    if (!(args == null)) {
+      return result;
+    }
 
     JsonArray array = args.getAsJsonArray();
     for (JsonElement jsonElement : array) {
@@ -33,15 +56,23 @@ public class ArgsParsing {
                                                 ParsingUtils.parseType(object.get("type").getAsString()),
                                                 descrText));
     }
+
+    return result;
   }
 
-  public static Declaration getOptions(JsonElement element) {
+  public static boolean isOption(String name) {
+    String trimedName = name.trim();
+    return trimedName.equals("options") || trimedName.equals("[options]");
+  }
+
+  public static Declaration getOptions(JsonElement element, boolean isRequired) {
     JsonElement options = element.getAsJsonObject().get("options");
-    if (options == null) {
-      return null;
-    }
 
     OptionDeclaration declaration = new OptionDeclaration();
+    declaration.setRequired(isRequired);
+    if (options == null) {
+      return declaration;
+    }
     for (JsonElement jsonElement : options.getAsJsonArray()) {
       declaration.addParameter(new MethodParameterDeclaration(Generator.getElement(jsonElement, "name"),
                                                               ParsingUtils.parseType(Generator.getElement(jsonElement, "type")),
@@ -51,4 +82,7 @@ public class ArgsParsing {
     return declaration;
   }
 
+  public static String[] argsFromName(String fullName) {
+    return fullName.substring(fullName.indexOf('(') + 1, fullName.lastIndexOf(')')).split(",");
+  }
 }
--- generator/src/kkey/generator/blocks/MethodParameterDeclaration.java
+++ generator/src/kkey/generator/blocks/MethodParameterDeclaration.java
@@ -3,13 +3,27 @@ package kkey.generator.blocks;
 
 public class MethodParameterDeclaration extends  Declaration {
   private final String myDescription;
+  private boolean myIsRequired;
 
-  public MethodParameterDeclaration(String name, String type, String description) {
+  public MethodParameterDeclaration(String name, String type, String description, boolean isRequired) {
     super(name, type);
     myDescription = description;
+    this.myIsRequired = isRequired;
+  }
+
+  public MethodParameterDeclaration(String name, String type, String description) {
+    this(name, type, description, true);
   }
 
   public String getDescription() {
     return myDescription;
+  }
+
+  public boolean isRequired() {
+    return myIsRequired;
+  }
+
+  public void setRequired(boolean isRequired) {
+    myIsRequired = isRequired;
   }
 }
